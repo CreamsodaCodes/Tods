d11b8c8a4f04de954356b725434e58769528d4dd
jojojoking <fkohler2003@gmail.com>
2019-04-16 18:09:56 +0200

    Update Regeln.cs

--- Tile Test/Assets/Skripts/Regeln.cs
+++ Tile Test/Assets/Skripts/Regeln.cs
@@ -4,11 +4,11 @@ using UnityEngine;
 
 public class Regeln : MonoBehaviour
 {
-    private string Regeln;
+    private string Regeln1;
     // Start is called before the first frame update
     void Start()
     {
-        Regeln = "";
+        Regeln1 = "";
     }
 
     // Update is called once per frame
