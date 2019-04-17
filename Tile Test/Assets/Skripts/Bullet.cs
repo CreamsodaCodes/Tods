833173224e8b2d37b0f970fcf2c21d5617167377
CreamsodaCodes <noahnsteiner@googlemail.com>
2019-04-17 13:03:10 +0200

    Test (Nicht vollständig)

--- Tile Test/Assets/Skripts/Bullet.cs
+++ Tile Test/Assets/Skripts/Bullet.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 
 public class Bullet : MonoBehaviour
 {   
-    Controller BulletController;
+    public Controller BulletController;
     [SerializeField]
     private float speed;
     [SerializeField]
@@ -14,13 +14,14 @@ public class Bullet : MonoBehaviour
     void Start()
     {
         rb.velocity = transform.right * speed;
-        Debug.Log(BulletController.Leben);
+        
         
     }
     void OnTriggerStay2D(Collider2D LOL)
     {
         Debug.Log(LOL.name);
         Destroy(gameObject);
+        Debug.Log(BulletController.Leben);
     }
         
 
