ca633dd641b26ec920a0a5f1a71a0927a853b130
CreamsodaCodes <noahnsteiner@googlemail.com>
2019-04-19 11:30:17 +0200

    Sichtfeld fertig
    Sichtfeld bei jedem helden mit Q akktivieren

---
+++ Tile Test/Assets/SichtfeldPlayer2.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SichtfeldPlayer2 : MonoBehaviour
+{
+    int call;
+    private SpriteRenderer mySichtfeld;
+    private BoxCollider2D myHitBox;
+    Controller TurnSpeicher;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        call = 10;
+        mySichtfeld = GetComponent<SpriteRenderer>();
+        myHitBox = GetComponent<BoxCollider2D>();
+        TurnSpeicher = GameObject.Find ("Player").GetComponent<Controller> ();
+        mySichtfeld.enabled = !mySichtfeld.enabled;
+        myHitBox.enabled = !myHitBox.enabled;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(TurnSpeicher.welcherSpieler == "Player2"){
+        SichtfeldAkttivator();
+        }
+        else{
+            myHitBox.enabled = false;
+            mySichtfeld.enabled = false;
+        }
+    }
+   void SichtfeldAkttivator(){
+       if(Input.GetKeyDown(KeyCode.Q)){
+            mySichtfeld.enabled = !mySichtfeld.enabled;
+            myHitBox.enabled = !myHitBox.enabled;
+        }
+   }
+}
