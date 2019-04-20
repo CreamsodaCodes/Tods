a9f732b29a0790e245d31d5502d8d882a5c87a40
CreamsodaCodes <noahnsteiner@googlemail.com>
2019-04-20 13:16:27 +0200

    Zufalls generator gestartet
    LOL

---
+++ Tile Test/Assets/Skripts/Warscheinlichkeit.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+using UnityEngine;
+
+public class Warscheinlichkeit : MonoBehaviour
+{
+    int Zufallsgenerator100;
+private void Start() {
+    Zufallsgenerator100 = Random.Range(1,100);
+}
+    private void Update() {
+        
+    }
+}
