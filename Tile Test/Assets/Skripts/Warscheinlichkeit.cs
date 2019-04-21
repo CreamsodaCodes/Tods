f3e625ec8574405fee64dfb843a71fd9e4b3f844
CreamsodaCodes <noahnsteiner@googlemail.com>
2019-04-21 19:05:48 +0200

    Obgetroffen funktion drin

--- Tile Test/Assets/Skripts/Warscheinlichkeit.cs
+++ Tile Test/Assets/Skripts/Warscheinlichkeit.cs
@@ -6,10 +6,20 @@ using UnityEngine;
 public class Warscheinlichkeit : MonoBehaviour
 {
     int Zufallsgenerator100;
+    int WarscheinlichkeitSchuss;
 private void Start() {
     Zufallsgenerator100 = Random.Range(1,100);
+    WarscheinlichkeitSchuss = 100;
 }
     private void Update() {
-        
+        ObGetroffen();
+    }
+    private void ObGetroffen(){
+     if(WarscheinlichkeitSchuss >= Zufallsgenerator100){
+         Debug.Log("Treffer!");
+     }
+     else{
+         Debug.Log("Verfehlt!");
+     }
     }
 }
